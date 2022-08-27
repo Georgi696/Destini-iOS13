0385218b772f4fd8f11ecde38d39e6c0dcfe087f
Georgi Aleksiev <georgi_aleksiev87@abv.bg>
2022-08-27 22:44:05 +0200

    added new functionality

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -13,10 +13,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
+    }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let userTrue = storyBrain.question(userAnswer)
+    }
+    
+    
+    func updateUI(){
+        
     }
 
 
